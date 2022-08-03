912f68b5a75121409120dc48f8081caaf9076793
DELL <nguyennavi0611@gmail.com>
2022-08-03 21:13:28 +0700

    Dung thay doi code

--- src/org/ali/MainClass.java
+++ src/org/ali/MainClass.java
@@ -3,5 +3,6 @@ package org.ali;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Them code thay doi");
+        System.out.println("Them code thay doi cua Dung");
     }
 }
