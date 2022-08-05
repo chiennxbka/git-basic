90f38063d2f412e3e36d89f5ec721b88e4217862
chiennx <chiennx@mintpotvn.com>
2022-08-05 20:12:48 +0700

    change command

--- src/org/ali/GitCommand.java
+++ src/org/ali/GitCommand.java
@@ -1,9 +1,11 @@
 package org.ali;
 
-public class GitAdd {
+public class GitCommand {
     public static void main(String[] args) {
-        System.out.println("Command: git add <_full_path_tenfile>");
-        System.out.println("Command: git commit -m \"Commit file\"");
-        System.out.println("Command: git push <ten branch>");
+        System.out.println("Command: git add <_full_path_tenfile>"); // add file vaof repository
+        System.out.println("Command: git commit -m \"Commit file\"");// Commit file voi message sau -m
+        System.out.println("Command: git pull");// update code ve may
+        System.out.println("Command: git push <ten branch>");// push to branch
+        System.out.println("Command: git branch");// liet ke danh sach branch hien co
     }
 }
