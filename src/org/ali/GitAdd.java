ad20f1a74c44e162e6c67d8ffe1ac14937015973
chiennx <chiennx@mintpotvn.com>
2022-08-05 19:43:39 +0700

    commit gile git add

---
+++ src/org/ali/GitAdd.java
@@ -0,0 +1,9 @@
+package org.ali;
+
+public class GitAdd {
+    public static void main(String[] args) {
+        System.out.println("Command: git add <_full_path_tenfile>");
+        System.out.println("Command: git commit -m \"Commit file\"");
+        System.out.println("Command: git push <ten branch>");
+    }
+}
